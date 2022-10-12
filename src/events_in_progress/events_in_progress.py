5049c00240695a94db7b314d40a8f9a248d4a962
Isaac Ingleby <isaac.ingleby@ecotricity.co.uk>
2022-10-12 21:35:27 +0100

    feat: placed almost all code into functions, this would mean any unit tests would be more straightforward.

--- src/events_in_progress/events_in_progress.py
+++ src/events_in_progress/events_in_progress.py
@@ -42,7 +42,7 @@ class VideoPlayEvent:
 
     def __post_init__(self):
         """SQLite has limited support for datetime, so I'm adding some functionality to manage it
-        via the class. This was mainly used in debugging stage
+        via the class. This was mainly used in early debugging stages.
         """
         self.start_dtime = datetime.fromtimestamp(self.start_time)
         self.end_dtime = datetime.fromtimestamp(self.end_time)
@@ -55,6 +55,14 @@ class VideoPlayEvent:
         """Returns core attributes for clearer formatting."""
 
         return self.index, self.start_time, self.end_time
+
+
+@dataclass
+class Column:
+    """A basic dataclass to store column names and datatype"""
+
+    name: str
+    datatype: str
 
 
 def get_dt_from_tstamp(timestamp: float) -> datetime:
@@ -154,13 +162,141 @@ def generate_event_group(event_path: Path) -> List[VideoPlayEvent]:
     return data_group
 
 
-def get_table_results(connection: Connection, table: str) -> str:
+def get_basic_stats(
+    full_events: List[VideoPlayEvent], plausible_events: List[VideoPlayEvent]
+) -> str:
+    """Output basic statistics for the user.
+
+    Args:
+        full_events: The raw VideoPlayEvent instances pulled from event records.
+        plausible_events: The VideoPlayEvent instances that pass business rules.
+
+    Returns:
+        Basic information about the results as a string.
+    """
+
+    # some very basic data analysis
+    min_start_time = min(event.start_time for event in plausible_events)
+    max_end_time = max(event.end_time for event in plausible_events)
+
+    # report output
+    return (
+        f"Full number of events: {len(full_events)}.\n"
+        f"Number of viable events: {len(plausible_events)}.\n"
+        f"Minimum event start time: {get_dt_from_tstamp(min_start_time)}.\n"
+        f"Maximum event end time: {get_dt_from_tstamp(max_end_time)}.\n"
+    )
+
+
+def drop_and_create_table(connection: Connection, table_name: str, columns: List[Column]) -> None:
+    """Automate the drop and creating steps for tables in the database.
+
+    Args:
+        connection: The SQLite connection with which the function will query the result.
+        table_name: The table to be set up.
+        columns: The column names and schema.
+    """
+
+    cursor = connection.cursor()
+
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    cursor.execute(
+        f"""
+    CREATE TABLE {table_name} (
+        {", ".join([f"{col.name} {col.datatype}" for col in columns])}
+    )     
+    """
+    )
+
+    connection.commit()
+
+
+def calculate_max_concurrent_events(
+    connection: Connection, source_table: str, target_table: str
+) -> None:
+    """Calculate maximum concurrent events using CTEs.
+
+    Args:
+        connection: The SQLite connection with which the function will query the result.
+        source_table: The "source" table from which the data will be generated.
+        target_table: The table into which the query will input data
+    """
+    cursor = connection.cursor()
+
+    cursor.execute(  # this logic will find overlaps in time and eliminate all else.
+        f"""
+      WITH events_with_date_intersects AS (
+        -- first we need to calculate the beginning and end of the event intersect
+        -- provided they intersect at all
+    SELECT r1.rn AS rnLeft,
+           r1.startTime AS startTimeLeft,
+           r1.endTime AS endTimeLeft,
+           r2.rn AS rnRight,
+           r2.startTime AS startTimeRight,
+           r2.endTime AS endTimeRight,
+           -- these CASE statements would need expanding to cover more scenarios in the real
+           -- world, but here they are adequate to pull out the correct dates
+           CASE
+             WHEN r2.startTime <= r1.endTime
+             THEN r2.startTime
+           END AS StartOfIntersect,
+           CASE
+             WHEN r1.endTime >= r2.startTime
+             THEN r1.endTime
+           END AS EndOfIntersect
+
+      FROM {source_table} AS r1
+      JOIN {source_table} AS r2  -- drop rows that don't intersect
+        ON r2.startTime <= r1.EndTime
+       AND r1.startTime <= r2.EndTime
+       AND r1.rn != r2.rn  -- don't include rows that intersect with themselves 
+    ),
+           event_with_most_date_intersects AS (
+        -- Now we will find the event that most frequently intersects other events  
+    SELECT rnLeft,
+           COUNT(rnLeft) AS frequency_count--,
+
+      FROM events_with_date_intersects
+  GROUP BY rnLeft
+  ORDER BY frequency_count DESC
+     LIMIT 1
+    ),
+           events_shared_time_period AS (
+        -- Now we find the maximum time_period all events covered that intersected the
+        -- most frequent event 
+    SELECT events.rnLeft,
+           MAX(events.StartOfIntersect) AS StartOfIntersect,
+           MIN(events.EndOfIntersect) AS EndOfIntersect
+
+      FROM events_with_date_intersects AS events
+      JOIN event_with_most_date_intersects AS event
+        ON event.rnLeft = events.rnLeft
+    )
+    INSERT INTO {target_table}
+        -- Insert events that intersect that along with some information on how they intersect 
+    SELECT ewdi.rnRight AS rn,
+           ewdi.startTimeRight AS eventStart,
+           ewdi.endTimeRight AS eventEnd,
+           estp.StartOfIntersect,
+           estp.EndOfIntersect
+
+      FROM events_shared_time_period AS estp
+      JOIN events_with_date_intersects AS ewdi
+        ON ewdi.rnLeft = estp.rnLeft
+       AND ewdi.StartOfIntersect <= estp.StartOfIntersect
+       AND ewdi.EndOfIntersect >= estp.EndOfIntersect
+    """
+    )
+    connection.commit()
+
+
+def get_table_results(connection: Connection, target_table: str) -> str:
     """Return the results of the table as a string. Meant to somewhat mimic the behaviour of Sparks
     `Dataframe.show()`.
 
     Args:
         connection: The SQLite connection with which the function will query the result.
-        table: The table you wish to query.
+        target_table: The table you wish to query.
 
     Returns:
         The query result as a string.
@@ -168,30 +304,30 @@ def get_table_results(connection: Connection, table: str) -> str:
     cursor = connection.cursor()
 
     # return results of query
-    cursor.execute(f"SELECT * FROM {table}")
+    cursor.execute(f"SELECT * FROM {target_table}")
     query_result = cursor.fetchall()
 
     # extract column names for formatting from table schema info
-    cursor.execute("PRAGMA table_info(events_max_simultaneous)")
+    cursor.execute(f"PRAGMA table_info({target_table})")
     columns_names = [column_info[1] for column_info in cursor.fetchall()]
 
     formatted_result = ""  # add to this string
 
-    # define the borders
+    # define the top and bottom borders
     column_title = f"{'-' * 20}+"
     border_string = f"+{column_title * 5}"
     formatted_dt_col_names = [f"{col}".ljust(20) for col in columns_names]
     column_name_str = f"|{'|'.join(formatted_dt_col_names)}|"
     formatted_result += f"{border_string}\n{column_name_str}\n{border_string}\n"
 
-    # define how the table contents are presented
+    # define how the table contents are presented (the actual result and borders)
     for row in query_result:
         formatted_result += (  # attempt to prettify the output somewhat
             f"|{str(row[0]).ljust(20)}|{get_dt_from_tstamp(row[1])} "
             f"|{get_dt_from_tstamp(row[2])} |{get_dt_from_tstamp(row[3])} "
             f"|{get_dt_from_tstamp(row[4])} |\n"
         )
-    formatted_result += border_string
+    formatted_result += border_string  # append a border to the bottom
 
     return formatted_result
 
@@ -207,117 +343,44 @@ def run():
         event for event in events if event.is_3_hours_or_less and event.start_proceeds_end
     ]
 
-    # some very basic data analysis for now, more complex logic to follow
-    # (and eventually be moved to their own functions)
-    min_start_time = min(event.start_time for event in cleaned_events)
-    max_end_time = max(event.end_time for event in cleaned_events)
-
-    # report output
-    print(
-        f"Full number of events: {len(events)}.\nNumber of viable events: {len(cleaned_events)}.\n"
-        f"Minimum event start time: {get_dt_from_tstamp(min_start_time)}.\n"
-        f"Maximum event end time: {get_dt_from_tstamp(max_end_time)}.\n"
-    )
+    basic_facts = get_basic_stats(events, cleaned_events)
+    print(basic_facts)
 
     with sqlite3.connect(directory_root / "events.db") as conn:  # connect to sqlite3 database
         cursor = conn.cursor()  # create a cursor
 
-        cursor.execute("DROP TABLE IF EXISTS events_raw")
-        cursor.execute(
-            """
-        CREATE TABLE events_raw (
-            rn INTEGER,
-            StartTime INTEGER,
-            EndTime INTEGER          
-        )
-        """
-        )
-
-        cursor.execute("DROP TABLE IF EXISTS events_max_simultaneous")
-        cursor.execute(
-            """
-        CREATE TABLE events_max_simultaneous (
-            rn INTEGER,
-            eventStart INTEGER,
-            eventEnd INTEGER,
-            StartOfIntersect INTEGER,
-            EndOfIntersect INTEGER
-        )
-        """
+        drop_and_create_table(
+            connection=conn,
+            table_name="events_raw",
+            columns=[
+                Column("rn", "INTEGER"),
+                Column("StartTime", "INTEGER"),
+                Column("EndTime", "INTEGER"),
+            ],
         )
 
-        cursor.executemany(  # insert all these events into the events.events table
+        drop_and_create_table(
+            connection=conn,
+            table_name="events_max_simultaneous",
+            columns=[
+                Column("rn", "INTEGER"),
+                Column("eventStart", "INTEGER"),
+                Column("eventEnd", "INTEGER"),
+                Column("StartOfIntersect", "INTEGER"),
+                Column("EndOfIntersect", "INTEGER"),
+            ],
+        )
+
+        cursor.executemany(  # SQLite syntax to insert these events into the events.events table
             f" INSERT INTO events_raw VALUES (?,?,?)",
             [event.return_core_attributes() for event in cleaned_events]
         )
 
-        cursor.execute(  # this logic will find overlaps in time and eliminate all else.
-            """
-          WITH events_with_date_intersects AS (
-            -- first we need to calculate the beginning and end of the event intersect
-            -- provided they intersect at all
-        SELECT r1.rn AS rnLeft,
-               r1.startTime AS startTimeLeft,
-               r1.endTime AS endTimeLeft,
-               r2.rn AS rnRight,
-               r2.startTime AS startTimeRight,
-               r2.endTime AS endTimeRight,
-               -- these CASE statements would need expanding to cover more scenarios in the real
-               -- world, but here they are adequate to pull out the correct dates
-               CASE
-                 WHEN r2.startTime <= r1.endTime
-                 THEN r2.startTime
-               END AS StartOfIntersect,
-               CASE
-                 WHEN r1.endTime >= r2.startTime
-                 THEN r1.endTime
-               END AS EndOfIntersect
-
-          FROM events_raw AS r1
-          JOIN events_raw AS r2  -- drop rows that don't intersect
-            ON r2.startTime <= r1.EndTime
-           AND r1.startTime <= r2.EndTime
-           AND r1.rn != r2.rn  -- don't include rows that intersect with themself 
-        ),
-               event_with_most_date_intersects AS (
-            -- Now we will find the event that most frequently intersects other events  
-        SELECT rnLeft,
-               COUNT(rnLeft) AS frequency_count--,
-
-          FROM events_with_date_intersects
-      GROUP BY rnLeft
-      ORDER BY frequency_count DESC
-         LIMIT 1
-        ),
-               events_shared_time_period AS (
-            -- Now we find the maximum time_period all events covered that intersected the
-            -- most frequent event 
-        SELECT events.rnLeft,
-               MAX(events.StartOfIntersect) AS StartOfIntersect,
-               MIN(events.EndOfIntersect) AS EndOfIntersect
-               
-          FROM events_with_date_intersects AS events
-          JOIN event_with_most_date_intersects AS event
-            ON event.rnLeft = events.rnLeft
+        calculate_max_concurrent_events(
+            connection=conn, source_table="events_raw", target_table="events_max_simultaneous"
         )
-        INSERT INTO events_max_simultaneous
-            -- Insert events that intersect that along with some information on how they intersect 
-        SELECT ewdi.rnRight AS rn,
-               ewdi.startTimeRight AS eventStart,
-               ewdi.endTimeRight AS eventEnd,
-               estp.StartOfIntersect,
-               estp.EndOfIntersect
-               
-          FROM events_shared_time_period AS estp
-          JOIN events_with_date_intersects AS ewdi
-            ON ewdi.rnLeft = estp.rnLeft
-           AND ewdi.StartOfIntersect <= estp.StartOfIntersect
-           AND ewdi.EndOfIntersect >= estp.EndOfIntersect
-        """
-        )
-        conn.commit()
 
-        table_result = get_table_results(connection=conn, table="events_max_simultaneous")
+        table_result = get_table_results(connection=conn, target_table="events_max_simultaneous")
         print(table_result)
 
 
